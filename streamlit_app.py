ccaea28e0a66e990297ce477b41cf4310077757b
Breeanajp <97449904+Breeanajp@users.noreply.github.com>
2024-10-07 01:12:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,21 @@
 import streamlit as st
+import sqlite3 as sq
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+
+
+
+st.title("3444 project")
+
+
+
+registerPage = st.Page("registerPage.py", title="Register Page", icon=":material/add_circle:")
+loginPage = st.Page("loginPage.py", title="Login Page", icon=":material/delete:")
+homePage = st.Page("homePage.py", title="Home Page", icon=":material/add_circle:")
+
+
+
+
+pg = st.navigation([registerPage, loginPage, homePage])
+
+pg.run()
+
